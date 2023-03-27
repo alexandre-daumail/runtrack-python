44b0c01d71ee0911413cb6cf9e8a3b2858aba988
Alexandre DAUMAIL <alexandre.daumail@laplateforme.io>
2023-03-27 09:19:46 +0200

    first commit

---
+++ job07/main.py
@@ -0,0 +1,12 @@
+def Add(a, b):
+    return a + b
+
+# Appel de la fonction Add avec différents paramètres
+resultat1 = Add(5, 10)
+resultat2 = Add(20, 30)
+resultat3 = Add(100, 200)
+
+# Affichage des résultats
+print("Résultat 1 : ", resultat1)
+print("Résultat 2 : ", resultat2)
+print("Résultat 3 : ", resultat3)
