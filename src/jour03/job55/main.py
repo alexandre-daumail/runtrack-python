714ffb9918fa833492b394c64b0a98db53a1f9c9
Alexandre DAUMAIL <alexandre.daumail@laplateforme.io>
2023-03-30 15:38:04 +0200

    psyduck found

--- src/jour03/job55/main.py
+++ src/jour03/job55/main.py
@@ -1,21 +1,30 @@
 import json
 
-encodings = ['utf-8', 'latin-1', 'cp1252']
+ENCODINGS = ['utf-8', 'latin-1', 'cp1252']
+POKEMON_FILE = 'pokemon.json'
+DATA_FILE = '../job01/data.txt'
 
-for encoding in encodings:
-    try:
-        with open('pokemon.json', 'r', encoding=encoding) as f:
-            data = json.load(f)
-            pokemons = []
-            for pokemon in data:
-                pokemons.append(pokemon['name']['french'])
-        print(f"Successfully loaded data using encoding {encoding}")
-        break
-    except UnicodeDecodeError:
-        print(f"Failed to load data using encoding {encoding}")
 
-with open('../job01/data.txt', 'r') as f:
-    data = f.read()
-    for pokemon in pokemons:
-        if pokemon in data:
-            print(f"{pokemon} found!")
+def load_pokemon_names():
+    for encoding in ENCODINGS:
+        try:
+            with open(POKEMON_FILE, 'r', encoding=encoding) as f:
+                data = json.load(f)
+                return [pokemon['name']['english'] for pokemon in data]
+        except UnicodeDecodeError:
+            pass
+    raise Exception("Failed to load Pokemon data using all encodings")
+
+
+def find_pokemon_names():
+    pokemons = load_pokemon_names()
+    with open(DATA_FILE, 'r') as f:
+        data = f.read()
+        words = data.split()
+        print('Looking for pokemon')
+        for pokemon in pokemons:
+            if pokemon in words:
+                print(f"{pokemon} found!")
+
+
+find_pokemon_names()
